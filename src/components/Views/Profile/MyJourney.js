0b5c10c9beaaf6466aa374869bd3bb4149f3bbf7
Radiant Juan <rjuan@buyster.local>
2022-10-02 18:25:47 +0800

    fixed experiences

--- src/components/Views/Profile/MyJourney.js
+++ src/components/Views/Profile/MyJourney.js
@@ -9,11 +9,11 @@ import {
 const MyJourney = () => {
     return (
         <React.Fragment>
-            <Col xs={12} lg={3}>
+            <Col xs={12}>
                 <Card className="animate__animated animate__fadeInDown animate__delay-1s">
                     <CardHeader>
                         <h4 className="m-0">
-                            Diversify (Temple and Webster client) - <span className="text-primary">Full-stack Engineer</span>
+                            Temple and Webster - <span className="text-primary">Full-stack Engineer</span>
                         </h4>
                         <p>
                             <small className="text-muted">Taguig, Philippines</small><br />
@@ -22,16 +22,16 @@ const MyJourney = () => {
                     </CardHeader>
                     <CardBody>
                         <p>
-                            With the position of Full-stack Engineer, I handle the development, implementation, management, optimization and continuous improvement of the Temple And Webster online furniture shop based in Australia.
+                            Temple and webster's product is an online store that sells, home, kitchen, and office tools and furnitures, more like an IKEA but online, they're company is based in Sydney Australia, and I'm work for them remotely with the help of Diversify offshore staffing solutions, I handle the development, implementation, management, optimization, and continuous improvement of their e-commerce website. As a team, we also have a strong process and currently using scrum.Temple and webster's product is an online store that sells, home, kitchen, and office tools and furnitures, more like an IKEA but online, they're company is based in Sydney Australia, and I'm work for them remotely with the help of Diversify offshore staffing solutions, I handle the development, implementation, management, optimization, and continuous improvement of their e-commerce website. As a team, we also have a strong process and currently using scrum.
                         </p>
                     </CardBody>
                 </Card>
             </Col>
-            <Col xs={12} lg={3}>
+            <Col xs={12}>
                 <Card className="animate__animated animate__fadeInDown animate__delay-1s">
                     <CardHeader>
                         <h4 className="m-0">
-                            Microsourcing (HungryHungry client) - <span className="text-primary">PHP Developer</span>
+                            HungryHungry  - <span className="text-primary">PHP Developer</span>
                         </h4>
                         <p>
                             <small className="text-muted">Pasig, Philippines</small><br />
@@ -40,13 +40,13 @@ const MyJourney = () => {
                     </CardHeader>
                     <CardBody>
                         <p>
-                            Back-end developer of Ordermate Online website, an online ordering multi-site application where merchant can freely self-manage their content for their online ordering site section,Ordermate Online is owned by an Australian Company based in Melbourne named Ordermate
-                            I do the back end development, API integration, third party app integration, server configuration for Ordermate Online and all back-end related.
+                            Hungryhungry is an online ordering system provider for restaurants in Australia, they have 500+ active clients currently, building an e-commerce website using their web application product with custom CMS called "hungyhungry admin" is one of their services, they also include as option a tablet application POS system called "HH Hub" where the customers manage online orders. I work for them remotely with the help of MicroSourcing Inc.
+                            My role as a PHP Developer is to develop, implement, manage, optimize and continuous improvement of the Hungryhungry admin, it's a full-stack development using back-end and front-end skills, I also do API development for the "HH Hub", I also do DevOps work but not limited to AWS EC2, RDS, and Cloud formation configuration, and linux.Hungryhungry is an online ordering system provider for restaurants in Australia, they have 500+ active clients currently, building an e-commerce website using their web application product with custom CMS called "hungyhungry admin" is one of their services, they also include as option a tablet application POS system called "HH Hub" where the customers manage online orders. I work for them remotely with the help of MicroSourcing Inc. My role as a PHP Developer is to develop, implement, manage, optimize and continuous improvement of the Hungryhungry admin, it's a full-stack development using back-end and front-end skills, I also do API development for the "HH Hub", I also do DevOps work but not limited to AWS EC2, RDS, and Cloud formation configuration, and linux.
                         </p>
                     </CardBody>
                 </Card>
             </Col>
-            <Col xs={12} lg={3}>
+            <Col xs={12}>
                 <Card className="animate__animated animate__fadeInDown animate__delay-1s">
                     <CardHeader>
                         <h4 className="m-0">
@@ -58,21 +58,16 @@ const MyJourney = () => {
                         </p>
                     </CardHeader>
                     <CardBody>
-                        <ul>
-                            <li>
-                                Transitioned from ABS-CBN to IBM
-                            </li>
-                            <li>
-                                Web developer for IBM’s ABS-CBN induction deck project
-                            </li>
-                            <li>
-                                Responsible for back-end development for all the websites of ABS-CBN’s TV network
-                            </li>
-                        </ul>
+                        <p>
+                            IBM has a subsidiary company called "IBM Solutions Delivery Inc." one of their projects is called Project "Donald" where their client is ABS-CBN one of the famous TV network in the Philippines, I was originally an employee of ABC-CBN and I was transitioned to IBM.
+
+                            With my role as an Applications Developer, I develop promotional websites from scratch one example is that if they have a movie that they are currently promoting or an album from one of their artists, I was also the sole developer for SkyCable's website which is the number one Cable and broadband provider in the Philippines.
+                        </p>
+
                     </CardBody>
                 </Card>
             </Col>
-            <Col xs={12} lg={3}>
+            <Col xs={12}>
                 <Card className="animate__animated animate__fadeInDown animate__delay-1s">
                     <CardHeader>
                         <h4 className="m-0">
@@ -84,14 +79,9 @@ const MyJourney = () => {
                         </p>
                     </CardHeader>
                     <CardBody>
-                        <ul>
-                            <li>
-                                Web Developer and Designer
-                            </li>
-                            <li>
-                                Email Marketing Advertisements Designer
-                            </li>
-                        </ul>
+                        <p>
+                            Affinity X is a Marketing and Design Company, My role as a web developer is to create mostly an email marketing, and create website occationally using wordpress, yola, joomla and drupal.
+                        </p>
                     </CardBody>
                 </Card>
             </Col>
