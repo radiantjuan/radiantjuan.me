80027aa2e69c834a626d445194720d57787fe758
radiantcjuan <radiantcjuan@gmail.com>
2023-09-29 23:23:47 +0800

    fixed spacing and added new information

--- src/components/Views/Profile/MyJourney.js
+++ src/components/Views/Profile/MyJourney.js
@@ -1,10 +1,5 @@
-import React from 'react';
-import {
-    Card,
-    CardHeader,
-    Col,
-    CardBody
-} from 'reactstrap';
+import React from "react";
+import { Card, CardHeader, Col, CardBody } from "reactstrap";
 
 const MyJourney = () => {
     return (
@@ -13,22 +8,204 @@ const MyJourney = () => {
                 <Card className="animate__animated animate__fadeInDown animate__delay-1s">
                     <CardHeader>
                         <h4 className="m-0">
-                            Temple and Webster - <span className="text-primary">Full-stack Engineer</span>
+                            Temple and Webster -{" "}
+                            <span className="text-primary">
+                                Full-stack Engineer
+                            </span>
                         </h4>
                         <p>
-                            <small className="text-muted">Taguig, Philippines</small><br />
-                            <small className="text-muted">JANUARY 25, 2021 - PRESENT</small>
+                            <small className="text-muted">
+                                Taguig, Philippines
+                            </small>
+                            <br />
+                            <small className="text-muted">
+                                JANUARY 25, 2021 - PRESENT
+                            </small>
                         </p>
                     </CardHeader>
                     <CardBody>
                         <p>
-                            Temple and webster's product is an online store that sells, home, kitchen, and office tools and furnitures, more like an IKEA but online, the company is based in Sydney Australia, I work for them remotely with the help of Diversify offshore staffing solutions.
+                            Temple and webster's product is an online store that
+                            sells, home, kitchen, and office tools and
+                            furnitures, more like an IKEA but online, the
+                            company is based in Sydney Australia, I work for
+                            them remotely with the help of Diversify offshore
+                            staffing solutions.
+                        </p>
+                        <p>Roles and responsibilities</p>
+                        <ul>
+                            <li>Full-stack engineer</li>
+                            <li>
+                                Development of new features and enhancements of
+                                T&W E-Commerce website
+                            </li>
+                            <li>
+                                Development and maintenance of CRM and admin
+                                tool
+                            </li>
+                            <li>
+                                Utilizing agile scrum practices for the team
+                            </li>
+                            <li>
+                                Composing knowledge base documentation for the
+                                team
+                            </li>
+                            <li>Task architectural planning and deployment</li>
+                            <li>Setup logging and monitoring in Kibana</li>
+                        </ul>
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col xs={12}>
+                <Card className="animate__animated animate__fadeInDown animate__delay-1s">
+                    <CardHeader>
+                        <h4 className="m-0">
+                            HungryHungry -{" "}
+                            <span className="text-primary">PHP Developer</span>
+                        </h4>
+                        <p>
+                            <small className="text-muted">
+                                Pasig, Philippines
+                            </small>
                             <br />
+                            <small className="text-muted">
+                                MAY 15, 2017 - JANUARY 22, 2021
+                            </small>
+                        </p>
+                    </CardHeader>
+                    <CardBody>
+                        <p>
+                            Hungryhungry is an online ordering system provider
+                            for restaurants in Australia, they have 500+ active
+                            clients currently, building an e-commerce website
+                            using their web application product with custom CMS
+                            called "hungyhungry admin" as one of their services,
+                            they also include a tablet application POS system
+                            called "HH Hub" where the customers manage online
+                            orders. I work for them remotely with the help of
+                            MicroSourcing Inc.
+                        </p>
+                        <p>Roles and responsibilities:</p>
+                        <i>
+                            *NOTE: The role title “PHP Developer” is general and
+                            has more responsibility which includes but not
+                            limited to
+                        </i>
+                        <ul>
+                            <li>PHP developer (Full-stack engineer)</li>
+                            <li>
+                                Development and enhancement of Hungryhungry
+                                Admin Back-end and front-end
+                            </li>
+                            <li>
+                                API Development for the Point of sale system
+                                mobile application called “HH Hub”
+                            </li>
+                            <li>Deployment Orchestration</li>
+                            <li>Agile Scrum usage and implementation</li>
+                            <li>
+                                DevOps responsibilities:
+                                <ul>
+                                    <li>AWS EC2 configuration</li>
+                                    <li>RDS configuration </li>
+                                    <li>Cloud formation</li>
+                                    <li>
+                                        Setup logging, monitoring, and alerts
+                                        using datadog
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col xs={12}>
+                <Card className="animate__animated animate__fadeInDown animate__delay-1s">
+                    <CardHeader>
+                        <h4 className="m-0">
+                            IBM (ABS-CBN Induction Project) -{" "}
+                            <span className="text-primary">
+                                Web Applications Developer
+                            </span>
+                        </h4>
+                        <p>
+                            <small className="text-muted">
+                                Quezon City, Philippines
+                            </small>
                             <br />
-                            I handle the development, implementation, management, optimization, and continuous improvement of their e-commerce website.
+                            <small className="text-muted">
+                                Mar 2015 - May 2017
+                            </small>
+                        </p>
+                    </CardHeader>
+                    <CardBody>
+                        <p>
+                            IBM has a subsidiary company called "IBM Solutions
+                            Delivery Inc." one of their projects is called
+                            Project "Donald" where their client is ABS-CBN one
+                            of the famous TV network in the Philippines, my
+                            first employer for ABS-CBN project is Octal
+                            Philippines and I was transitioned to IBM.
+                        </p>
+                        <p>Roles and responsibilities:</p>
+                        <ul>
+                            <li>Web applications developer</li>
+                            <li>
+                                TV network promotional websites development
+                                which includes but not limited to:
+                                <ul>
+                                    <li>Movie website promotions</li>
+                                    <li>Fan base website promotions</li>
+                                    <li>Album website promotions</li>
+                                </ul>
+                            </li>
+                            <li>
+                                Development and enhancement of web
+                                application/tools such as
+                                <ul>
+                                    <li>Custom content management system</li>
+                                    <li>
+                                        Fan based voting system for awards like
+                                        “push” awards
+                                    </li>
+                                    <li>
+                                        Music web application like spotify like
+                                        “one music PH”
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                Development and enhancements of Cable and
+                                Broadband provider called SkyCable
+                            </li>
+                        </ul>
+                    </CardBody>
+                </Card>
+            </Col>
+            <Col xs={12}>
+                <Card className="animate__animated animate__fadeInDown animate__delay-1s">
+                    <CardHeader>
+                        <h4 className="m-0">
+                            Octal Philippines, Inc. -{" "}
+                            <span className="text-primary">Web Developer</span>
+                        </h4>
+                        <p>
+                            <small className="text-muted">
+                                Quezon City, Philippines
+                            </small>
                             <br />
-                            <br />
-                            As a team, we also have a strong process and currently using scrum.
+                            <small className="text-muted">
+                                Sept 7, 2015 - Feb 29, 2017
+                            </small>
+                        </p>
+                    </CardHeader>
+                    <CardBody>
+                        <p>
+                            In my role as a web developer for ABS-CBN project, I
+                            was the sole developer for SkyCable's website which
+                            is the number one Cable and broadband provider in
+                            the Philippines. I do enhancements, maintenance, and
+                            additional feature for https://www.mysky.com.ph/
                         </p>
                     </CardBody>
                 </Card>
@@ -37,19 +214,30 @@ const MyJourney = () => {
                 <Card className="animate__animated animate__fadeInDown animate__delay-1s">
                     <CardHeader>
                         <h4 className="m-0">
-                            HungryHungry  - <span className="text-primary">PHP Developer</span>
+                            Affinity X -{" "}
+                            <span className="text-primary">Web Developer</span>
                         </h4>
                         <p>
-                            <small className="text-muted">Pasig, Philippines</small><br />
-                            <small className="text-muted">MAY 15, 2017 - JANUARY 22, 2021</small>
+                            <small className="text-muted">
+                                Quezon City, Philippines
+                            </small>
+                            <br />
+                            <small className="text-muted">
+                                Sept 8, 2014 - Aug 28, 2015
+                            </small>
                         </p>
                     </CardHeader>
                     <CardBody>
                         <p>
-                            Hungryhungry is an online ordering system provider for restaurants in Australia, they have 500+ active clients currently, building an e-commerce website using their web application product with custom CMS called "hungyhungry admin" as one of their services, they also include a tablet application POS system called "HH Hub" where the customers manage online orders. I work for them remotely with the help of MicroSourcing Inc.
-                            <br />
-                            <br />
-                            My role as a PHP Developer is to develop, implement, manage, optimize and continuous improvement of the Hungryhungry admin, it's a full-stack development using back-end and front-end skills, I also do API development for the "HH Hub", I also do DevOps work but not limited to AWS EC2, RDS, and Cloud formation configuration, and linux.
+                            Affinity X is a Marketing and Design Company which
+                            provides services like marketing and promotional
+                            design materials for their client based in US,
+                            Serbia, and India.
+                        </p>
+                        <p>
+                            My role as a web developer is to create mostly an
+                            email marketing, and create website occationally
+                            using wordpress, yola, joomla and drupal.
                         </p>
                     </CardBody>
                 </Card>
@@ -58,44 +246,38 @@ const MyJourney = () => {
                 <Card className="animate__animated animate__fadeInDown animate__delay-1s">
                     <CardHeader>
                         <h4 className="m-0">
-                            IBM (ABS-CBN Induction Project) - <span className="text-primary">Web Applications Developer</span>
+                            Coca-cola Bottler’s Business Services -{" "}
+                            <span className="text-primary">Web Developer</span>
                         </h4>
                         <p>
-                            <small className="text-muted">Quezon City, Philippines</small><br />
-                            <small className="text-muted">SEPTEMBER 2015 - APRIL 2017</small>
+                            <small className="text-muted">
+                                Taguig City, Philippines
+                            </small>
+                            <br />
+                            <small className="text-muted">
+                                Jun 1, 2014 - Sept 5, 2014
+                            </small>
                         </p>
                     </CardHeader>
                     <CardBody>
-                        <p>
-                            IBM has a subsidiary company called "IBM Solutions Delivery Inc." one of their projects is called Project "Donald" where their client is ABS-CBN one of the famous TV network in the Philippines, I was originally an employee of ABC-CBN and I was transitioned to IBM.
-                            <br />
-                            <br />
-                            With my role as an Applications Developer, I develop promotional websites from scratch one example is that if they have a movie that they are currently promoting or an album from one of their artists, I was also the sole developer for SkyCable's website which is the number one Cable and broadband provider in the Philippines.
-                        </p>
-
-                    </CardBody>
-                </Card>
-            </Col>
-            <Col xs={12}>
-                <Card className="animate__animated animate__fadeInDown animate__delay-1s">
-                    <CardHeader>
-                        <h4 className="m-0">
-                            Affinity X - <span className="text-primary">Web Developer</span>
-                        </h4>
-                        <p>
-                            <small className="text-muted">Quezon City, Philippines</small><br />
-                            <small className="text-muted">SEPTEMBER 2014 - SEPTEMBER 2015</small>
-                        </p>
-                    </CardHeader>
-                    <CardBody>
-                        <p>
-                            Affinity X is a Marketing and Design Company, My role as a web developer is to create mostly an email marketing, and create website occationally using wordpress, yola, joomla and drupal.
-                        </p>
+                        <ul>
+                            <li>
+                                Production support for Coca-cola's portal
+                                website.
+                            </li>
+                            <li>
+                                Debugging Coca-cola's portal website using
+                                ASP.NET in Visual Basic
+                            </li>
+                            <li>
+                                Data Analysis and management using MS SQL Server
+                            </li>
+                        </ul>
                     </CardBody>
                 </Card>
             </Col>
         </React.Fragment>
     );
-}
+};
 
 export default MyJourney;
